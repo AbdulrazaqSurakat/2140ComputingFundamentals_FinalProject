ade9db4f9f6d5e94aee20621a72735b757153bd1
HaydenRosenberg <haydenrosenberg10@gmail.com>
2022-11-27 12:43:57 -0500

    base class for all of the items that are sold

---
+++ items.py
@@ -0,0 +1,8 @@
+
+class Items:
+    """this class stores data about all of the items that are sold
+    """
+
+    def __init__(self, cost, calories) -> None:
+        self.cost = 0
+        self.calories = 0
