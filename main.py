144cc1ab01e3acec6ca37ee157ce1ac65a8f08d4
HaydenRosenberg <haydenrosenberg10@gmail.com>
2022-12-07 19:44:24 -0500

    added comments

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ from menu_view import Menu_view
 from cart import Cart
 from controller import Controller
 
+#creates the cart that will store all of the ordered items
 theCart = Cart()
+
+#creats the menu view so the program can get its first input
 view = Menu_view(theCart)
+
+#create the controller that interacts with the view and the cart
 theController = Controller(theCart, view)
 
+#controller runs the program until user wants to end ordering
 theController.run_menu()
